82956ed1644838481f5e72742e9d11b11eb2f0cc
tory99 <hjw990826@gmail.com>
2025-02-05 15:32:14 +0900

    mariadb.query 추가

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ let server = require('./server.js');
 let router = require('./router.js');
 let requesthandler = require('./requesthandler.js');
 
+const mariadb = require('./database/connect/mariadb.js');
+mariadb.connect();
+
 server.start(router.route, requesthandler.handle);
--- requesthandler.js
+++ requesthandler.js
@@ -1,15 +1,24 @@
+const mariadb = require('./database/connect/mariadb.js');
+
 function main(response) {
     console.log('main');
+
+    mariadb.query("SELECT * FROM product", function(err, rows) {
+        console.log(rows);
+    })
+
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('Main!');
     response.end();    
 }
 function login(response) {
     console.log('login');
+
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('Login');
     response.end();  
 }
+
 
 
 
